ecefc490ff55ffd7572fba08cb8773a72bb242ac
Esteban <estebanarivasv@gmail.com>
2022-05-26 03:28:07 -0300

    feat: add initial project

---
+++ src/main/java/com/erivas/acs/AcsApplication.java
@@ -0,0 +1,14 @@
+package com.erivas.acs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class AcsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcsApplication.class, args);
+	}
+
+}
